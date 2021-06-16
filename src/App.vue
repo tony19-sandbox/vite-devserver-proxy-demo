880ea2a69acb688bbcccb04e6142e37f0e4d11c5
Tony Trinh <tony19@gmail.com>
2021-06-16 15:16:51 -0500

    render posts

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <ul>
+    <li v-for="post in posts" :key="post.id">{{ post.text }}</li>
+  </ul>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+const resp = await fetch('/posts')
+const posts = await resp.json()
 </script>
 
 <style>
