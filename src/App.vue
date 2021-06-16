d0c5b17959c45ec453ef88951caddc785ebe135f
Tony Trinh <tony19@gmail.com>
2021-06-16 15:47:59 -0500

    render posts

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
+  <h1>Posts</h1>
   <ul>
     <li v-for="post in posts" :key="post.id">{{ post.text }}</li>
   </ul>
 </template>
 
-<script setup>
-const resp = await fetch('/posts')
-const posts = await resp.json()
+<script>
+import { ref, onMounted } from 'vue'
+
+export default {
+  setup() {
+    const posts = ref([])
+
+    onMounted(async () => {
+      const resp = await fetch('/posts')
+      posts.value = await resp.json()
+    })
+
+    return { posts }
+  }
+}
+
 </script>
 
 <style>
